23508f6b1a2cbbf130df05b37eee8ac2e6d29720
PacoFMD <pacomonfmd@hotmail.com>
2020-03-28 19:03:58 -0500

    for play store

--- Assets/Scripts/Google Services/AdManager.cs
+++ Assets/Scripts/Google Services/AdManager.cs
@@ -7,8 +7,8 @@ public class AdManager : MonoBehaviour
 {
     string APP_ID = "ca-app-pub-3075864074697091~2852795131";
 
-    //string BANNER_ID_TEST = "ca-app-pub-3075864074697091/5950851322"; // este es el chido para la app
-    string BANNER_ID_TEST = "ca-app-pub-3940256099942544/5224354917"; // este viene desde Google Ads Test
+    string BANNER_ID_TEST = "ca-app-pub-3075864074697091/5950851322"; // este es el chido para la app
+    //string BANNER_ID_TEST = "ca-app-pub-3940256099942544/5224354917"; // este viene desde Google Ads Test
 
     RewardBasedVideoAd rewardedVideo;
 
@@ -17,7 +17,7 @@ public class AdManager : MonoBehaviour
     {
 #if UNITY_ANDROID
         //Cuando ya se va a publicar la app
-        //MobileAds.Initialize(APP_ID);
+        MobileAds.Initialize(APP_ID);
         RequestVideoAd();
 # endif
     }
@@ -31,15 +31,15 @@ public class AdManager : MonoBehaviour
 
     void RequestVideoAd()
     {
-        //string BANNER_ID_TEST = "ca-app-pub-3075864074697091/5950851322"; // este es el chido para la app
-        string BANNER_ID_TEST = "ca-app-pub-3940256099942544/5224354917"; // este viene desde Google Ads Test
+        string BANNER_ID_TEST = "ca-app-pub-3075864074697091/5950851322"; // este es el chido para la app
+        //string BANNER_ID_TEST = "ca-app-pub-3940256099942544/5224354917"; // este viene desde Google Ads Test
         rewardedVideo = RewardBasedVideoAd.Instance;
 
         //Para la app
-        //AdRequest adrequest = new AdRequest.Builder().Build();
+        AdRequest adRequest = new AdRequest.Builder().Build();
 
         //Temporal
-        AdRequest adRequest = new AdRequest.Builder().AddTestDevice("33BE2250B43518CCDA7DE426D04EE231").Build();
+        //AdRequest adRequest = new AdRequest.Builder().AddTestDevice("33BE2250B43518CCDA7DE426D04EE231").Build();
 
         rewardedVideo.LoadAd(adRequest, BANNER_ID_TEST);
 
